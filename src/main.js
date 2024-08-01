f4cd98105b8850d43a339474a6199a06a115e852
Bitnara <qlcskfk02@gmail.com>
2024-08-01 15:51:57 +0900

    플러그인 만들기

--- src/main.js
+++ src/main.js
@@ -3,8 +3,18 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import funcPlugins from './plugins/func';
+import objPlugins from './plugins/obj';
+import person from './plugins/person';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+app.use(funcPlugins);
+app.use(objPlugins, { name: 'bitnara' });
+app.use(person);
+
+app.use(router);
+app.mount('#app');
+
 import 'bootstrap/dist/js/bootstrap.js';
 
 // console.log(import.meta.env.MODE);
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,23 +6,39 @@
     <AppGrid :items="items" v-slot="{ item }" col-class="col-6">
       <AppCard>{{ item }}</AppCard>
     </AppGrid>
+    <!-- <h2>{{ $person.name }}</h2> -->
     <button class="btn btn-primary my-4" @click="goAboutPage">About</button>
+    <button class="btn btn-primary ms-1" @click="person.say">
+      click person
+    </button>
   </div>
 </template>
+
+<script>
+export default {
+  created() {
+    // console.log(this.$person.name);
+    // this.$person.say();
+  },
+};
+</script>
 
 <script setup>
 import { useRouter } from 'vue-router';
 import { useRoute } from 'vue-router';
 import AppGrid from '@/components/AppGrid.vue';
 import AppCard from '@/components/AppCard.vue';
+import { inject } from 'vue';
 
 const route = useRoute();
 const router = useRouter();
+const person = inject('person');
 
 const items = [
   'name: ' + route.name,
   'path: ' + route.path,
   'params: ' + route.params.value,
+  'person: ' + person.name,
 ];
 
 const goAboutPage = () => {
