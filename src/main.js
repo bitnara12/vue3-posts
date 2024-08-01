df72403c30c38d559c27ce979553c358e805f9e8
bitnara-opasnet <84299110+bitnara-opasnet@users.noreply.github.com>
2024-08-01 21:28:35 +0900

    custom directives 생성

--- src/main.js
+++ src/main.js
@@ -5,11 +5,12 @@ import App from './App.vue';
 import router from './router';
 import person from './plugins/person';
 // import globalComponents from './plugins/global-components';
+import globalDirectives from './plugins/global-directives';
 
 const app = createApp(App);
 // app.use(globalComponents);
+app.use(globalDirectives);
 app.use(person);
-
 app.use(router);
 app.mount('#app');
 
